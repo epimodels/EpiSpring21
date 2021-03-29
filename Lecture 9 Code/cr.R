9e0059bcd65e01a821d57562bde6579558bec2a4
Eric Lofgren <Eric.Lofgren@wsu.edu>
2021-03-29 13:04:16 -0700

    Lec 9

---
+++ Lecture 9 Code/cr.R
@@ -0,0 +1,21 @@
+library(survival)
+library(survminer)
+mean <- 5
+rate_time <- 1/mean
+
+time_one <- rexp(500, rate = rate_time)
+time_two <- rexp(500, rate = rate_time)
+times <- as.data.frame(cbind(time_one,time_two))
+
+times$event_time <- NA
+times$event_time <- pmin(times$time_one,times$time_two)
+
+times$event <- "Censored"
+times$event[times$time_one<times$time_two] <- "One"
+times$event[times$time_one>times$time_two] <- "Two"
+times$event[times$event_time>10] <- "Censored"
+times$event <- as.factor(times$event)
+
+crfit <- survfit(Surv(event_time, event) ~ 1, data=times)
+plot(crfit,col=c("Red3","Blue"))
+ggcompetingrisks(crfit)
